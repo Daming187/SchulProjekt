25084f93de5fa243bbc09f82f0ccd3029c7af53e
Damian <122474264+Daming187@users.noreply.github.com>
2023-11-09 09:07:35 +0100

    Add files via upload

---
+++ LESCHNER-LN/InformationsSystem/Leschner.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: leschner
+ * Date: 09.11.2023
+ * Time: 08:55
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+// ggf. weitere #using-Direktive
+namespace InformationsSystem
+{
+	public class Leschner
+	{
+		//ATTRIBUTE
+		//KONSTRUKTOREN
+		public Leschner()
+		{
+			// Dieser Konstruktor bleibt "leer"
+		}
+		//METHODEN
+		public void Ausführen()
+		{
+			Console.WriteLine("Aufruf von Ausführen()");
+		}
+	}
+}
---
+++ LESCHNER-LN/InformationsSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: leschner
+ * Date: 09.11.2023
+ * Time: 08:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace InformationsSystem
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Programm starten
+			Console.WriteLine("Informationssystem\n**********************************\n");
+			
+			//Aufruf der Methode "Ausführen()" im instanziierten Klassenobjekt
+			// "leschner" der projektspezifischen Klase "Leschner"
+			Leschner leschner = new Leschner();
+			leschner.Ausführen();
+			
+			// Programm anhalten
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
